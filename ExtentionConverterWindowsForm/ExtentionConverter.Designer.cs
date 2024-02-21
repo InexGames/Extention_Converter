58c4e0b3900454226db595e65cc2b9feee896b10
Inex <31888517+ezel3050@users.noreply.github.com>
2024-02-22 00:42:59 +0330

    init

---
+++ ExtentionConverterWindowsForm/ExtentionConverter.Designer.cs
@@ -0,0 +1,125 @@
+﻿namespace ExtentionConverterWindowsForm
+{
+    partial class ExtentionConverter
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(ExtentionConverter));
+            this.pathTextBox = new System.Windows.Forms.TextBox();
+            this.extentionTextBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // pathTextBox
+            // 
+            this.pathTextBox.Location = new System.Drawing.Point(14, 57);
+            this.pathTextBox.Name = "pathTextBox";
+            this.pathTextBox.Size = new System.Drawing.Size(470, 20);
+            this.pathTextBox.TabIndex = 0;
+            // 
+            // extentionTextBox
+            // 
+            this.extentionTextBox.Location = new System.Drawing.Point(113, 138);
+            this.extentionTextBox.Name = "extentionTextBox";
+            this.extentionTextBox.Size = new System.Drawing.Size(270, 20);
+            this.extentionTextBox.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(235, 24);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(42, 20);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Path";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(215, 105);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(76, 20);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Extention";
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(207, 199);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(102, 37);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Process";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label3
+            // 
+            this.label3.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Tai Le", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(11, 250);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(262, 14);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Developed by Inex - inexcompany.ceo@gmail.com";
+            // 
+            // ExtentionConverter
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(496, 272);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.extentionTextBox);
+            this.Controls.Add(this.pathTextBox);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "ExtentionConverter";
+            this.Text = "ExtentionConverter";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox pathTextBox;
+        private System.Windows.Forms.TextBox extentionTextBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ ExtentionConverterWindowsForm/ExtentionConverter.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ExtentionConverterWindowsForm
+{
+    public partial class ExtentionConverter : Form
+    {
+        string defaultPath = "C:\\Users\\sadra\\Downloads";
+        string defaultExtention = "png";
+
+        public ExtentionConverter()
+        {
+            InitializeComponent();
+            pathTextBox.Text = defaultPath;
+            extentionTextBox.Text = defaultExtention;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string directoryPath = pathTextBox.Text;
+            string newExtension = extentionTextBox.Text;
+
+            if (string.IsNullOrEmpty(directoryPath))
+            {
+                MessageBox.Show("Please enter a directory path.");
+                return;
+            }
+
+            if (!Directory.Exists(directoryPath))
+            {
+                MessageBox.Show("Directory not found.");
+                return;
+            }
+
+            try
+            {
+                string[] files = Directory.GetFiles(directoryPath);
+
+                foreach (string file in files)
+                {
+                    string filename = Path.GetFileNameWithoutExtension(file);
+                    string newFilePath = Path.Combine(directoryPath, filename + "." + newExtension);
+                    File.Move(file, newFilePath);
+                }
+                Console.WriteLine("File extensions changed successfully!");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error: " + ex.Message);
+            }
+        }
+
+  
+    }
+}
